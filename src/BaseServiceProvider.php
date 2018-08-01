2afef7a0861be0c94856eb682694bf8970adb2ad
dandi <dandi.webdev@gmail.com>
2018-08-01 22:59:15 +0700

    adding config, route, and layouts view

---
+++ src/BaseServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Webcore\Page;
+
+use Illuminate\Support\ServiceProvider;
+
+class BaseServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        require __DIR__.'/routes.php';
+
+        $this->loadViewsFrom(__DIR__.'/views', 'base');
+
+        $this->publishes([
+            __DIR__.'/config' => config_path('webcore')
+        ], 'config');
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
---
+++ src/routes.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Http\Request;
+use League\Glide\ServerFactory;
+use League\Glide\Responses\LaravelResponseFactory;
+use Illuminate\Contracts\Filesystem\Filesystem;
+
+Route::get('/img/{path}', function(Filesystem $filesystem, $path) {
+    $server = ServerFactory::create([
+        'response' => new LaravelResponseFactory(app('request')),
+        'source' => $filesystem->getDriver(),
+        'cache' => $filesystem->getDriver(),
+        'cache_path_prefix' => '.cache',
+        'base_url' => 'img',
+    ]);
+
+    return $server->getImageResponse($path, request()->all());
+
+})->where('path', '.*');
